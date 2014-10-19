a0f753a17a1f76eedac8b84c51c8becb06fc0e2b
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-10-19 12:14:56 +0200

    bind must be in hiera

--- manifests/redis.pp
+++ manifests/redis.pp
@@ -1,9 +1,7 @@
 class flapjack_base::redis {
 
-  Class['flapjack_base'] -> Class['flapjack_base::redis']
-
   class {'redis_base':
-    conf_bind => hiera('flapjack_base::base_redis_bind',$flapjack_base::base_redis_bind)
+    conf_bind => hiera('flapjack_base::base_redis_bind')
   }
 
 }
