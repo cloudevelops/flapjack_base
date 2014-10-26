76ca13c7879a266579e4ab88fb294e78ecd5921e
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-10-26 13:56:02 +0100

    simplified

--- manifests/processor.pp
+++ manifests/processor.pp
@@ -2,28 +2,30 @@ define flapjack_base::processor (
   $counter = 0,
 ) {
 
-  $newcounter = $counter + $name
+#  $newcounter = $counter + $name
   notify { "Name: ${name}, Counter: ${counter}, Newcounter: ${newcounter}": }
   if $name == 1 {
 
-    notify { "Now is 1: ${newcounter}": }
+    notify { "Now is 1, doing nothing": }
 
-#    file { "/etc/flapjack/flapjack_processor_${newcounter}.yaml":
-#      content => template('flapjack_base/etc/flapjack/flapjack_processor.yaml.erb'),
-#      require => Class['flapjack_base'];
-#    }
-
-#    file { "/etc/init/flapjack_processor_${newcounter}.conf":
-#      content => template('flapjack_base/etc/init/flapjack_processor.conf.erb'),
-#      require => [ Class['flapjack_base'], File["/etc/flapjack/flapjack_processor_${newcounter}.yaml"] ];
-#    }
   } else {
+    file { "/etc/flapjack/flapjack_processor_${newcounter}.yaml":
+      content => template('flapjack_base/etc/flapjack/flapjack_processor.yaml.erb'),
+      require => Class['flapjack_base'];
+    }
+
+    file { "/etc/init/flapjack_processor_${newcounter}.conf":
+      content => template('flapjack_base/etc/init/flapjack_processor.conf.erb'),
+      require => [ Class['flapjack_base'], File["/etc/flapjack/flapjack_processor_${newcounter}.yaml"] ];
+    }
+
+
     $dec = $name - 1
     $hash = {
-      "${dec}" => {
-        "counter" => $newcounter,
+      "${dec}" => {}
+#        "counter" => $newcounter,
       }
-    }
+#    }
 
     create_resources('processor', $hash)
 
