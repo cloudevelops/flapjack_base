5b14a2da9652142a5795c6817d49881e397423f4
Zdenek Janda <zdenek.janda@cloudevelops.com>
2014-10-26 17:12:06 +0100

    added new collectd dataa

--- manifests/monitoring/collectd.pp
+++ manifests/monitoring/collectd.pp
@@ -6,6 +6,33 @@ class flapjack_base::monitoring::collectd {
     keys => {
       events_queued => {
         type => 'gauge'
+      },
+      all_entities => {
+        type => 'gauge'
+      },
+      failing_entities => {
+        type => 'gauge'
+      },
+      all_checks => {
+        type => 'gauge'
+      },
+      failing_checks => {
+        type => 'gauge'
+      },
+      total_keys => {
+        type => 'gauge'
+      },
+      'processed_events/all_time/total' => {
+        type => 'derive'
+      },
+      'processed_events/all_time/ok' => {
+        type => 'derive'
+      },
+      'processed_events/all_time/failure' => {
+        type => 'derive'
+      },
+      'processed_events/all_time/action' => {
+        type => 'derive'
       }
     }
   }
